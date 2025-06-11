1ddcd0f1264be1a1d80b708602d7405ca090c6e9
Shinichiro Yabu <scin.ichi@gmail.com>
2025-06-11 14:46:08 +0900

    📝 利用規約を詳しく

--- src/routes/tos/+page.svelte
+++ src/routes/tos/+page.svelte
@@ -2,13 +2,27 @@
 	<title>スズメツールズ 利用規約</title>
 </svelte:head>
 
-<main class="pt-2 pb-4 text-gray-500 py-6 sm:py-8 lg:py-12">
+<main class="pt-2 pb-4 text-gray-500 py-6 sm:py-8 lg:py-12 body-text">
 	<header class="mb-5">
 		<h1 class="text-gray-800 text-3xl leading-7 font-bold">利用規約</h1>
 	</header>
 
-	<ol class="mb-6 sm:text-lg md:mb-8">
+	<ol class="sm:text-lg">
 		<li><a href="https://suzume.dev">スタジオスズメ</a>が提供するスズメツールズはどなたでも無償でご利用いただけます。</li>
-		<li>スズメツールズを利用したことによる損害などについては一切責任を負いません。</li>
 	</ol>
+
+	<section class="my-6 md:my-8">
+		<h2 class="text-gray-800 text-lg font-bold">免責事項</h2>
+		<ol class="sm:text-lg">
+			<li>スズメツールズの内容は、できる限り正確に提供していますが、その内容について一切保証するものではありません。</li>
+			<li>スズメツールズを利用したことによる損害などについては一切責任を負いません。</li>
+		</ol>
+	</section>
+
+	<section class="my-6 md:my-8">
+		<h2 class="text-gray-800 text-lg font-bold">プライバシーポリシー</h2>
+		<ol class="sm:text-lg">
+			<li>スズメツールズでは一切利用者の情報を収集しません。</li>
+		</ol>
+	</section>
 </main>
